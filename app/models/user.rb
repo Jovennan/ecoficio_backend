00addafff9c07a017ddb7d12e0678f8c4aca9795
Jovennan <jovennan@gmail.com>
2018-08-18 23:35:14 -0300

    permitir sign_up  sem definir um departamento para o user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :departamento
+  belongs_to :departamento, optional: true
 
   has_one :chefia_de_orgao, class_name: 'Orgao', foreign_key: :chefe_id
   
