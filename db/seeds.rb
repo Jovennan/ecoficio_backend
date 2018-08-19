8e2e6ec776af1b350cf346d2a1cbf87f36f90cec
Jovennan <jovennan@gmail.com>
2018-08-18 23:07:59 -0300

    Dados iniciais no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+orgao = Orgao.create(nome: "Polícia Militar", endereco: "Praça Pedro Américo, Centro - João Pessoa")
+departamento = Departamento.create(nome: "Estado Maior", orgao: Orgao.first)
+user = User.create(email: 'coordenador@pm.pb.gov.br',nome: "Beltrano", cpf: "09876543210",
+    cargo: "Chefe", password: '12345678', password_confirmation: '12345678',
+    departamento: Departamento.first)
